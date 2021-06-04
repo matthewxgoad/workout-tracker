b127387620370a6a058842d52279afd51a88275c
Matthew Goad <matthewxgoad@gmail.com>
2021-06-04 18:02:51 -0500

    updated html routes

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const dotenv = require("dotenv")
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./controllers/index.js")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
